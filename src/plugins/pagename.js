756e5ee7b6895620779a4d987c2b3e5c91feb840
Thales Alves <thales@thalesalv.es>
2022-08-22 22:03:51 +0100

    fixed titles

--- src/plugins/pagename.js
+++ src/plugins/pagename.js
@@ -1,9 +1,14 @@
-function titlePlugin(config) {
+const titlePlugin = config => {
     return (hook, vm) => {
         hook.doneEach((content) => {
             var pageTitle = document.URL.split('/').slice(-1)[0];
             pageTitle = pageTitle === '' ? 'Home' : pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1);
-            document.title = `${pageTitle}${config.suffix}`.replace('_', ' ');
+            document.title = titleCase(`${pageTitle}${config.suffix}`);
         });
     }
 }
+
+const titleCase = title => title.split(/ /g)
+        .map(word => `${word.substring(0, 1).toUpperCase()}${word.substring(1)}`)
+        .join(" ")
+        .replaceAll('_', ' ');
