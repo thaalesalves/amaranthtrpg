7248d1c71f3ea9bfd785ca439d633c7a18494073
Thales Alves <thales@thalesalv.es>
2022-08-24 23:03:30 +0100

    added custom page title plugin

--- src/plugins/pagename.js
+++ src/plugins/pagename.js
@@ -1,14 +1,14 @@
-const titlePlugin = config => {
-    return (hook, vm) => {
-        hook.doneEach((content) => {
-            var pageTitle = document.URL.split('/').slice(-1)[0];
-            pageTitle = pageTitle === '' ? 'Home' : pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1);
-            document.title = titleCase(`${pageTitle}${config.suffix}`);
-        });
-    }
+const titlePlugin = config => (hook, vm) => {
+    hook.doneEach(content => {
+        var pageTitleCustomDefined = document.body.outerHTML.toString()
+                .split('<!-- PageTitle:').pop()
+                .split('-->')[0].trim();
+
+        var pageTitle = !pageTitleCustomDefined.startsWith('<') ? pageTitleCustomDefined : titleCase(document.URL.split('/').slice(-1)[0]);
+        pageTitle = pageTitle === '' ? 'Home' : pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1);
+        document.title = `${pageTitle}${config.suffix}`;
+    });
 }
 
-const titleCase = title => title.split(/ /g)
-        .map(word => `${word.substring(0, 1).toUpperCase()}${word.substring(1)}`)
-        .join(" ")
-        .replaceAll('_', ' ');
+const titleCase = title => title.replace(/_/g, ' ')
+        .replace(/^([a-z])|[ (]+([a-z])/g, l => l.toUpperCase());
